ca08f42c66e9f80fbd436f0a4df60c36a36a5066
Pranab Dey <android.dey@gmail.com>
2019-02-12 16:08:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,109 @@
 !(function() {
-    var btn = document.querySelector('.getLinks'),
-        input = document.querySelector('.urlInput'),
-        links;
+    var btn,
+        n,
+        p = document.querySelector('.urlsBtn'),
+        single = document.querySelector('.single'),
+        multiple = document.querySelector('.multiple'),
+        input = document.querySelector('.input'),
+        textarea = document.querySelector('.textarea'),
+        put = document.querySelector('#output');
 
-    links = () => {
-        var put = document.querySelector('#output');
+    var links = () => {
         put.innerHTML = 'Fetching the video informations...';
-        fetch(`https://ripsave.com/download?video=${input.value}`)
-            .then(d => d.text())
-            .then(d => {
-                put.innerHTML = d;
-                var linkBtn = document.querySelectorAll('.download .table a');
-                linkBtn.forEach(d => {
-                    if (!d.classList.contains('btn-success')) {
-                        d.classList.add('btn-success');
+        //   var urls = ['https://ripsave.com/download?video=https://vimeo.com/305839334','https://ripsave.com/download?video=https://vimeo.com/86359067','https://ripsave.com/download?video=https://vimeo.com/131777414'];
+        n = document.querySelector(".urlInput");
+        var urlSplit = n.value.split(',').map(d => d.trim());
+        var urls = urlSplit.map(d => 'https://ripsave.com/download?video=' + d);
+        Promise.all(urls.map(d => fetch(d)))
+            .then(d => d.map(d => d.text()))
+            .then(d => d.map(d => {
+                put.innerHTML = '';
+                d.then(d => {
+                    var p = document.createElement('p'),
+                        heading = document.createElement('h3');
+                    p.innerHTML = d;
+                    p.className = 'vimeoVideos';
+                    var metaNode = Array.from(p.querySelectorAll('.vimeoVideos meta')),
+                        linkNode = Array.from(p.querySelectorAll('.vimeoVideos link')),
+                        scriptNode = Array.from(p.querySelectorAll('.vimeoVideos script')),
+                        table = Array.from(p.querySelectorAll('table a')),
+                        allNode = [...metaNode, ...linkNode, ...scriptNode];
+                    allNode.forEach(d => d.remove());
+                    table.forEach(d => d.classList.add('btn', 'btn-primary'));
+                    console.log(titleNode);
+                    heading.className = 'alert alert-primary vimeoVideosTitle';
+                    if (p.querySelector('.download') === null) {
+                        put.innerHTML = 'Please check the URL once. <br>It returns null means nothing is in there.'
                     }
-                });
-            })
-            .catch(err => console.log(`Error is: ${err.name} and Error message is: ${err.message}`));
+                    put.appendChild(heading);
+                    put.appendChild(p);
+                    var titleNode = Array.from(p.querySelectorAll('.vimeoVideos title'));
+                    //console.log(titleNode);
+                    titleNode.forEach(d => heading.innerHTML = d.innerHTML.split('Download: ')[1]);
+                })
+            }))
+            .catch(err => {
+                if (err.message === 'Failed to fetch') {
+                    var msg = 'Failed to fetch the video.<br>Please recheck the url and try again.';
+                    put.innerHTML = '<p class="alert alert-danger">Error Message:<br> ' + msg + '</p>';
+                } else {
+                    put.innerHTML = 'Error: ' + err.name + '<br>Error Message: ' + err.message;
+                }
+            });
     }
 
-    btn.onclick = links;
+    single.onclick = () => {
+        if (!single.classList.contains('active')) {
+            put.innerHTML = 'Copy the link and paste and click the button.';
+            //input.value = '';
+            single.classList.add('active');
+            multiple.classList.remove('active');
+            //     input.classList.remove('hide');
+            //     textarea.classList.add('hide');
+            p.innerHTML = '<input type="text" class="form-control urlInput input" placeholder="Enter video url" aria-label="Enter video url" aria-describedby="button-addon2"><div class="input-group-append"><button class="btn btn-secondary getLinks" type="button" id="button-addon2">Get Links</button></div>';
+        }
+        btn = document.querySelector('.getLinks');
+        btn.onclick = () => {
+            n = document.querySelector(".urlInput");
+            if (n.value.length === 0) {
+                put.innerHTML = 'Please enter the url';
+            } else if (n.value.length > 0 && !n.value.match(/(vimeo.com)/gi)) {
+                put.innerHTML = 'This is not Vimeo\'s url<br>Example: \'https://vimeo.com/86359067\'';
+            } else {
+                links();
+            }
+        }
+    }
+    multiple.onclick = () => {
+        if (!multiple.classList.contains('active')) {
+            put.innerHTML = 'Copy the link and paste and click the button.';
+            multiple.classList.add('active');
+            single.classList.remove('active');
+            //     textarea.classList.remove('hide');
+            //     input.classList.add('hide');
+            p.innerHTML = '<textarea name="urls" cols="30" rows="5" class="form-control urlInput textarea" aria-label="Enter videos url" aria-describedby="button-addon2"></textarea><div class="input-group-append"><button class="btn btn-secondary getLinks" type="button" id="button-addon2">Get Links</button></div>';
+        }
+        btn = document.querySelector('.getLinks');
+        btn.onclick = () => {
+            n = document.querySelector(".urlInput");
+            if (n.value.length === 0) {
+                put.innerHTML = 'Please enter the url';
+            } else if (n.value.length > 0 && !n.value.match(/(vimeo.com)/gi)) {
+                put.innerHTML = 'This is not Vimeo\'s url<br>Example: \'https://vimeo.com/86359067\'';
+            } else {
+                links();
+            }
+        }
+    }
+
+    document.querySelector('.getLinks').onclick = () => {
+        n = document.querySelector(".urlInput");
+        if (n.value.length === 0) {
+            put.innerHTML = 'Please enter the url';
+        } else if (n.value.length > 0 && !n.value.match(/(vimeo.com)/gi)) {
+            put.innerHTML = 'This is not Vimeo\'s url<br>Example: \'https://vimeo.com/86359067\'';
+        } else {
+            links();
+        }
+    };
 })();
